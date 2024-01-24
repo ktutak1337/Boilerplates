6db25ac66c2b6309cbd900040ff7683953d26c1b
ktutak1337 <ktutak1337@gmail.com>
2024-01-24 10:41:34 +0100

    Fixed script

--- Proxmox/template/cloud-init-proxmox-template.sh
+++ Proxmox/template/cloud-init-proxmox-template.sh
@@ -8,7 +8,6 @@ URL="https://cloud-images.ubuntu.com/minimal/releases/jammy/release/ubuntu-22.04
 IMAGE_NAME="ubuntu-22.04-minimal-cloudimg-amd64.img"
 NEW_SIZE="40G"
 STORAGE_POOL="local-lvm"
-IMAGE_FORMAT="qcow2"
 
 # VM config
 VM_ID=9000
@@ -38,8 +37,8 @@ qm create $VM_ID --memory $MEMORY --core $CORES --name $VM_NAME --net0 $NETWORK_
 echo "> Resizing the disk image '$IMAGE_NAME' to a new size of $NEW_SIZE"
 qemu-img resize $IMAGE_NAME $NEW_SIZE
 
-echo "> Importing the disk '$IMAGE_NAME' for virtual machine $VM_ID into the Proxmox VE storage pool '$STORAGE_POOL' with format '$IMAGE_FORMAT'"
-qm importdisk $VM_ID $IMAGE_NAME $STORAGE_POOL --format $IMAGE_FORMAT
+echo "> Importing the disk '$IMAGE_NAME' for virtual machine $VM_ID into the Proxmox VE storage pool '$STORAGE_POOL'"
+qm importdisk $VM_ID $IMAGE_NAME $STORAGE_POOL
 
 echo "> Configuring SCSI settings for the virtual machine $VM_ID disk in Proxmox VE. Using virtio-scsi-pci as the SCSI controller, assigning the disk from the storage pool '$STORAGE_POOL' with specific options (ssd=1, discard=on, iothread=1)."
 qm set $VM_ID --scsihw virtio-scsi-pci --scsi0 $STORAGE_POOL:vm-$VM_ID-disk-0,ssd=1,discard=on,iothread=1
